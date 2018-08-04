d771c9adcc51e53eb0aa1bfc397995a0707b185c
澔榕 于 <YUHAORONG@126.com>
2018-08-04 10:14:04 +0800

    .

--- Einstein/Einstein/Einstein.cpp
+++ Einstein/Einstein/Einstein.cpp
@@ -28,10 +28,54 @@ typedef struct pos {
 class situation {
 public:
 	int board[5][5];
+	pos positions[2][7];
+	int alive_cnt[2];//false À¶ true ºì
 	bool player;
 	situation *father = NULL, *brother = NULL, *son = NULL;
 	int cnt_visit = 0, cnt_win = 0;
-private:
-	pos positions[2][7];
-	int alive_cnt[2];//false À¶ true ºì
+	situation(int m[5][5], bool player)
+	{
+		for(int i=0;i<5;i++)
+			for (int j = 0; j < 5; j++)
+			{
+				board[i][j] = m[i][j];
+				if (m[i][j] > 0)
+				{
+					positions[1][m[i][j]] = pos(i, j);
+					alive_cnt[1]++;
+				}
+				if (m[i][j] < 0)
+				{
+					positions[0][-m[i][j]] = pos(i, j);
+					alive_cnt[0]++;
+				}
+			}
+		this->player = player;
+	}
+	situation(const situation &old)
+	{
+		for (int i = 0; i < 5; i++)
+			for (int j = 0; j < 5; j++)
+				board[i][j] = old.board[i][j];
+		for (int i = 1; i <= 6; i++)
+		{
+			positions[0][i] = old.positions[0][i];
+			positions[1][i] = old.positions[1][i];
+		}
+	}
+	situation(const situation& old, int piece, int moveNo)
+	{
+		*this = situation(old);
+		move(*this, piece, moveNo);
+	}
+
 };
+
+void move(situation &cur, int piece, int moveNo)
+{
+	cur.player = !cur.player;
+	int nx, ny;
+
+	cur.board[cur.positions[cur.player][piece].x][cur.positions[cur.player][piece].y] = 0;
+
+}
