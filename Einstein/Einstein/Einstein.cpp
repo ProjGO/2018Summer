0c57809b624db705efb42f13e33f26fbe198a028
澔榕 于 <YUHAORONG@126.com>
2018-08-04 09:20:04 +0800

    .

--- Einstein/Einstein/Einstein.cpp
+++ Einstein/Einstein/Einstein.cpp
@@ -14,3 +14,24 @@ const int blueMove[3][2] = {
 	-1,-1,
 	0,-1
 };
+
+bool ourColor = false;
+int Board[5][5];
+char name[50] = "AI";
+
+typedef struct pos {
+	int x, y;
+	pos(int x=0, int y=0) :x(x), y(y) {};
+	bool operator == (const pos &rhs) { return x == rhs.x&&y == rhs.y; }
+} pos;
+
+class situation {
+public:
+	int board[5][5];
+	bool player;
+	situation *father = NULL, *brother = NULL, *son = NULL;
+	int cnt_visit = 0, cnt_win = 0;
+private:
+	pos positions[2][7];
+	int alive_cnt[2];//false À¶ true ºì
+};
